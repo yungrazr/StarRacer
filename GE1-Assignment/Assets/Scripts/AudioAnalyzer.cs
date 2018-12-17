82637343b00caf30668630902a81a594574eed8c
Daniel Vegera <daniel.vegera@gmail.com>
2018-12-17 16:21:37 +0000

    added audio analysis and hud elements

---
+++ GE1-Assignment/Assets/Scripts/AudioAnalyzer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioAnalyzer : MonoBehaviour
+{
+    public GameObject audioSource;
+    AudioSource a;
+
+    public static int frameSize = 512;
+    public static float[] spectrum;
+    public static float[] bands;
+
+    public float binWidth;
+    public float sampleRate;
+
+    /*
+     * 20-60 - Subbase
+     * 60-250 - Bass
+     * 250-500 - Low midrange
+     * 500 - 2Khz - Midrange
+     * 2Khz - 4Khz - Upper midrange
+     * 4Khz - 6Khz - Presence
+     * 6Khz - 20Khz - Brilliance
+     */
+
+    // Use this for initialization
+    void Start()
+    {
+        sampleRate = AudioSettings.outputSampleRate;
+        binWidth = AudioSettings.outputSampleRate / 2 / frameSize;
+
+        a = audioSource.GetComponent<AudioSource>();
+        spectrum = new float[frameSize];
+        bands = new float[(int)Mathf.Log(frameSize, 2)];
+    }
+
+    void GetFrequencyBands()
+    {
+        for (int i = 0; i < bands.Length; i++)
+        {
+            int start = (int)Mathf.Pow(2, i) - 1;
+            int width = (int)Mathf.Pow(2, i);
+            int end = start + width;
+            float average = 0;
+            for (int j = start; j < end; j++)
+            {
+                average += spectrum[j] * (j + 1);
+            }
+            average /= (float)width;
+            bands[i] = average;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
+        GetFrequencyBands();
+    }
+}
--- GE1-Assignment/Assets/Scripts/RaceTrack.cs
+++ GE1-Assignment/Assets/Scripts/RaceTrack.cs
@@ -5,28 +5,40 @@ public class RaceTrack : MonoBehaviour
 {
     public float scale = 0.1f;
     public float speed = 1.0f;
-    float noiseStrength = 1f;
-    float noiseWalk = 1f;
-
-    private Vector3[] baseHeight;
 
     public GameObject audioSource;
     private AudioSource musicTrack;
     private float trackLength;
 
+    private Vector3[] baseHeight;
+
+    private Mesh mesh;
+    private MeshCollider colliderMesh;
+    private Renderer m_Renderer;
+
+    float color = 0;
+
     void Start()
     {
+        m_Renderer = GetComponent<Renderer>();
+        mesh = GetComponent<MeshFilter>().mesh;
+        colliderMesh = GetComponent<MeshCollider>();
+
         musicTrack = audioSource.GetComponent<AudioSource>();
         trackLength = musicTrack.clip.length;
-        Debug.Log(trackLength);
-        this.gameObject.transform.localScale = new Vector3(trackLength*10, this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
+        Debug.Log("track length in sec:" + trackLength);
+        this.gameObject.transform.localScale = new Vector3(trackLength * 10 * Time.deltaTime * 60, this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
     }
 
 
     void Update()
     {
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
-        MeshCollider colliderMesh = GetComponent<MeshCollider>();
+        color += 0.1f;
+        if(color>1.0f)
+        {
+            color = 0;
+        }
+        m_Renderer.material.color = Color.HSVToRGB(color, 1, (float)AudioAnalyzer.bands[0]);
 
         if (baseHeight == null)
             baseHeight = mesh.vertices;
@@ -36,7 +48,6 @@ public class RaceTrack : MonoBehaviour
         {
             Vector3 vertex = baseHeight[i];
             vertex.y += Mathf.Sin(Time.time * speed + baseHeight[i].x) * scale;
-            //vertex.y += Mathf.PerlinNoise(baseHeight[i].x + noiseWalk, baseHeight[i].y + Mathf.Sin(Time.time * 0.1f)) * noiseStrength;
             vertices[i] = vertex;
         }
         mesh.vertices = vertices;
