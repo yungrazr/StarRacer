8d98a7aeb028a89988cab48da3c5ce3886130923
Daniel Vegera <daniel.vegera@gmail.com>
2018-12-17 20:24:19 +0000

    first playable prototype

--- GE1-Assignment/Assets/Scripts/Player.cs
+++ GE1-Assignment/Assets/Scripts/Player.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public GameObject pCamera;
+    public GameObject trail1, trail2;
+
 
     Rigidbody rb;
     const float playerSpeed = 10;
-    const float playerStrafeSpeed = 15;
+    const float playerStrafeSpeed = 20;
 
     RaycastHit hit;
     float dist;
     Vector3 direction;
-
-    Vector3 frontPos = new Vector3(0, 0, 0);
-    Vector3 backPos = new Vector3(0, 0, 0);
 
     // Use this for initialization
     void Start () {
@@ -24,28 +23,27 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-
-        //Continuous acceleration
-        rb.AddRelativeForce(Vector3.forward * playerSpeed * Time.deltaTime * 60);
+        Vector3 frontPos = new Vector3(0, 0, 0);
+        Vector3 backPos = new Vector3(0, 0, 0);
+        rb.AddRelativeForce(Vector3.forward * playerSpeed * Time.deltaTime);
 
         //Key inputs to move left/right
         if (Input.GetKey(KeyCode.D))
         {
             rb.AddRelativeForce(Vector3.right * playerStrafeSpeed * Time.deltaTime * 30);
-            rb.transform.localRotation = Quaternion.Slerp(rb.transform.localRotation, Quaternion.Euler(0, 0, 15), Time.deltaTime * 5);
+            rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, Quaternion.Euler(0, 0, 15), Time.deltaTime * 5);
 
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             rb.AddRelativeForce(Vector3.left * playerStrafeSpeed * Time.deltaTime * 30);
-            rb.transform.localRotation = Quaternion.Slerp(rb.transform.localRotation, Quaternion.Euler(0, 0, -15), Time.deltaTime * 5);
+            rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, Quaternion.Euler(0, 0, -15), Time.deltaTime * 5);
         }
 
         if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
-            rb.transform.localRotation = Quaternion.Slerp(rb.transform.localRotation, Quaternion.Euler(0, 0, 0), Time.deltaTime * 5);
+            rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, Quaternion.Euler(0, 0, 0), Time.deltaTime * 5);
         }
 
         dist = Mathf.Infinity;
@@ -56,7 +54,7 @@ public class Player : MonoBehaviour {
 
         if (Physics.Raycast(transform.position, direction, out hit, dist))
         {
-            rb.transform.position = new Vector3(rb.transform.position.x, Mathf.Lerp(rb.transform.position.y, hit.point.y + 2, Time.deltaTime / 0.1f), rb.transform.position.z);
+            rb.transform.position = new Vector3(rb.transform.position.x, Mathf.Lerp(rb.transform.position.y, hit.point.y + 2, Time.deltaTime / 0.001f), rb.transform.position.z);
         }
         if (Physics.Raycast(posFront, direction, out hit, dist))
         {
@@ -66,17 +64,18 @@ public class Player : MonoBehaviour {
         {
             backPos = hit.point;
         }
-        /*
         if (frontPos.y > backPos.y)
         {
             float angle = Vector3.Angle(frontPos, backPos);
-            rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, Quaternion.Euler(angle, rb.transform.rotation.y, rb.transform.rotation.z), 1);
+            rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, Quaternion.Euler(angle*2, 0, 0), Time.deltaTime * 5);
         }
         if (frontPos.y < backPos.y)
         {
             float angle = Vector3.Angle(frontPos, backPos);
-            rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, Quaternion.Euler(-angle, rb.transform.rotation.y, rb.transform.rotation.z), 1);
+            rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, Quaternion.Euler(-angle*2, 0, 0), Time.deltaTime * 5);
         }
-        */
+
+
+
     }
 }
--- GE1-Assignment/Assets/Scripts/RaceTrack.cs
+++ GE1-Assignment/Assets/Scripts/RaceTrack.cs
@@ -10,8 +10,6 @@ public class RaceTrack : MonoBehaviour
     private AudioSource musicTrack;
     private float trackLength;
 
-    private Vector3[] baseHeight;
-
     private Mesh mesh;
     private MeshCollider colliderMesh;
     private Renderer m_Renderer;
@@ -27,7 +25,7 @@ public class RaceTrack : MonoBehaviour
         musicTrack = audioSource.GetComponent<AudioSource>();
         trackLength = musicTrack.clip.length;
         Debug.Log("track length in sec:" + trackLength);
-        this.gameObject.transform.localScale = new Vector3(trackLength * 10 * Time.deltaTime * 60, this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
+        //this.gameObject.transform.localScale = new Vector3(trackLength * 10 * Time.deltaTime * 60, this.gameObject.transform.localScale.y, this.gameObject.transform.localScale.z);
     }
 
 
@@ -38,16 +36,13 @@ public class RaceTrack : MonoBehaviour
         {
             color = 0;
         }
-        m_Renderer.material.color = Color.HSVToRGB(color, 0.5f, (float)AudioAnalyzer.bands[1]);
+        m_Renderer.material.color = Color.HSVToRGB(color, 1f, (float)AudioAnalyzer.bands[1]);
 
-        if (baseHeight == null)
-            baseHeight = mesh.vertices;
-
-        Vector3[] vertices = new Vector3[baseHeight.Length];
+        Vector3[] vertices = new Vector3[mesh.vertices.Length];
         for (int i = 0; i < vertices.Length; i++)
         {
-            Vector3 vertex = baseHeight[i];
-            vertex.y += Mathf.Sin(Time.time * speed + baseHeight[i].x) * scale;
+            Vector3 vertex = mesh.vertices[i];
+            vertex.y += Mathf.Sin(Time.time * speed + mesh.vertices[i].z) * (float)AudioAnalyzer.bands[1];
             vertices[i] = vertex;
         }
         mesh.vertices = vertices;
